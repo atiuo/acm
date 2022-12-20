100a9ca6eb56edc0c9fda0d1952c11ef2a7adceb
atiuo <wbohan1997@163.com>
2022-12-20 16:30:07 +0800

    update

---
+++ 1220-max_raindrop/01-solution.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int maxArea(vector<int>& height) {
+    int i = 0, j = height.size() - 1, res = 0;
+    while(i < j) {
+        res = height[i] < height[j] ? 
+            max(res, (j - i) * height[i++]): 
+            max(res, (j - i) * height[j--]); 
+    }
+    return res;
+}
+
+int main(){
+    vector<int> heights = {1,8,6,2,5,4,8,3,7};
+    int res = maxArea(heights);
+    cout << res << endl;
+}
