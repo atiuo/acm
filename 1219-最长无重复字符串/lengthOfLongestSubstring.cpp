15e0aed51a7014754b791c643cee286e5789ca31
atiuo <wbohan1997@163.com>
2022-12-20 13:36:11 +0800

    add

--- 1219-最长无重复字符串/lengthOfLongestSubstring.cpp
+++ 1219-最长无重复字符串/lengthOfLongestSubstring.cpp
@@ -10,7 +10,7 @@ void getLongestSubstring(string s) {
     int n = s.size();
 
     // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
-    int rk = -1;
+    int end = -1;
 
     // 最重要返回的：子串长度数值，子串内容
     int ans = 0;
@@ -22,9 +22,9 @@ void getLongestSubstring(string s) {
             // 左指针向右移动一格，移除一个字符
             occ.erase(s[i - 1]);
         }
-        while (rk + 1 < n && !occ.count(s[rk + 1])) {
+        while (end + 1 < n && !occ.count(s[end + 1])) {
             // 不断地移动右指针
-            occ.insert(s[rk + 1]);
+            occ.insert(s[end + 1]);
             ++rk;
         }
         // 第 i 到 rk 个字符是一个极长的无重复字符子串
